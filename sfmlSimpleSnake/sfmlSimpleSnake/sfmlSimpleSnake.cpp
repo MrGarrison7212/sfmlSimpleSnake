fcf8cc1d70f4c246a40cc4c97d6775db6f17b07c
MrGarrison7212 <bokipfc1992@gmail.com>
2022-02-26 03:06:15 +0100

    Inserting data - textures.

--- sfmlSimpleSnake/sfmlSimpleSnake/sfmlSimpleSnake.cpp
+++ sfmlSimpleSnake/sfmlSimpleSnake/sfmlSimpleSnake.cpp
@@ -15,6 +15,15 @@ int h = size * M;
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(w, h), "SFML Works");
+	//insert textures
+	sf::Texture t1, t2, t3, t4, t5, t6;
+	t1.loadFromFile("Data/background.png");
+	t2.loadFromFile("Data/fruit.png");
+	t3.loadFromFile("Data/snake.png");
+	t4.loadFromFile("Data/snake1.png");
+	t5.loadFromFile("Data/tail.png");
+
+	sf::Sprite sBackground(t1), sFruit(t2), sSnake(t3), sSnake1(t4), sTail(t5);
 
 	while (window.isOpen()) {
 

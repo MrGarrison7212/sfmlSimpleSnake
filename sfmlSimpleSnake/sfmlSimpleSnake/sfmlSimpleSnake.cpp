92d85d7ca25225b8357f93cd39ec8298eac5265b
MrGarrison7212 <bokipfc1992@gmail.com>
2022-02-26 03:25:00 +0100

    Adjusting movement logics.

--- sfmlSimpleSnake/sfmlSimpleSnake/sfmlSimpleSnake.cpp
+++ sfmlSimpleSnake/sfmlSimpleSnake/sfmlSimpleSnake.cpp
@@ -29,6 +29,11 @@ void gameLogic() {
 		s[i].x = s[i - 1].x;
 		s[i].y = s[i - 1].y; 
 	}
+	if (dir == 0) { s[0].y += 1; }
+	if (dir == 1) { s[0].y -= 1; }
+	if (dir == 2) { s[0].x += 1; }
+	if (dir == 3) { s[0].x -= 1; }
+
 }
 
 int main()
